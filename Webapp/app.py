647999be65c3269e000b0dcdfde7e67040f9fac4
evagpm <evagpm98@gmail.com>
2022-02-23 18:21:25 -0800

    Update app.py
    Added functions to display marriage age table

--- Webapp/app.py
+++ Webapp/app.py
@@ -7,6 +7,13 @@ export FLASK_ENV=development; flask run
 
 from flask import Flask, render_template, g, request
 
+import pandas as pd
+import censusdata
+
+import plotly.io as pio
+from plotly import express as px
+from plotly.io import write_html
+
 app = Flask(__name__)
 
 #Show base page with links to submit and view
@@ -30,7 +37,47 @@ def gather_data(request):
 
     return table
 
+def get_tables(codes, names, years):
+    tables = []
+    for year in years:
+        #Get table
+        df = censusdata.download('acs5', year,
+                   censusdata.censusgeo([('state', '*')]),
+                    codes)
+        
+        #Rename columns
+        name_dict = dict(zip(codes, names))
+        name_dict['index'] = 'State'
+        df = df.reset_index() #Turns row names into row
+        df = df.rename(columns = name_dict)
+        
+        #Shorten states column to state name
+        df = df.astype({'State':'str'})
+        df['State'] = df['State'].str.split(':').str.get(0) 
+        
+        #Add column for year
+        df['Year'] = year
+        
+        tables.append(df)
+    return pd.concat(tables)
+
+def make_marriage_table():
+    marriage = get_tables(['B12007_001E', 'B12007_002E'], 
+        ['Male age', 'Female age'], [2009, 2014, 2019])
+    mar_cal = marriage[marriage['State'] == "California"]
+    fig = px.scatter(data_frame = mar_cal, 
+                x = "Year", 
+                y = ["Male age", "Female age"],
+                title = "Median Age of First Marriage (CA)",
+                trendline = "ols", # ordinary least squares regression trendline
+                width = 800,
+                height = 600)
+    write_html(fig, "templates/plot.html")
+
+
+
 #Establish view page
 @app.route("/view")
 def view():
-    return render_template('view.html')
+    make_marriage_table()
+    return render_template('plot.html')

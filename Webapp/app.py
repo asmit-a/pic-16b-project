d9f502cac51b6c4bd227eb02b152ae485493d848
evagpm <evagpm98@gmail.com>
2022-02-23 20:00:55 -0800

    Update app.py
    Added in state selection

--- Webapp/app.py
+++ Webapp/app.py
@@ -25,25 +25,30 @@ def main():
 @app.route("/submit", methods=['POST', 'GET'])
 def submit():
     if request.method == 'GET' :
-        return render_template("submit.html")
+        states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 
+            'Colorado', 'Delaware', 'District of Columbia', 'Connecticut', 
+            'Florida', 'Georgia', 'Idaho', 'Hawaii', 'Illinois', 'Indiana', 
+            'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
+            'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+            'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
+            'New Jersey', 'New Mexico', 'New York', 'North Carolina', 
+            'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+            'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
+            'Texas', 'Vermont', 'Utah', 'Virginia', 'Washington', 
+            'West Virginia', 'Wisconsin', 'Wyoming', 'Puerto Rico']
+        return render_template("submit.html", states = states)
     else:
         table = request.form["table"]
+        state = request.form["state"]
         if (table == "marriage"):
-            make_marriage_table()
+            make_marriage_table(state)
             return render_template("marriage.html")
         elif (table == "household"):
-            make_household_table()
+            make_household_table(state)
             return render_template("household.html")
         elif (table == "divorce"):
-            make_divorce_table()
+            make_divorce_table(state)
             return render_template("divorce.html")
-
-#Function inserts the message into the table
-def gather_data(request):
-    #Get inputs from form
-    table = request.form['table']
-
-    return table
 
 def get_tables(codes, names, years):
     tables = []
@@ -69,29 +74,29 @@ def get_tables(codes, names, years):
         tables.append(df)
     return pd.concat(tables)
 
-def make_marriage_table():
+def make_marriage_table(state):
     marriage = get_tables(['B12007_001E', 'B12007_002E'], 
         ['Male age', 'Female age'], [2009, 2014, 2019])
-    mar_cal = marriage[marriage['State'] == "California"]
-    fig = px.scatter(data_frame = mar_cal, 
+    mar_state = marriage[marriage['State'] == state]
+    fig = px.scatter(data_frame = mar_state, 
                 x = "Year", 
                 y = ["Male age", "Female age"],
-                title = "Median Age of First Marriage (CA)",
+                title = "Median Age of First Marriage (" + state + ")",
                 trendline = "ols", # ordinary least squares regression trendline
                 width = 800,
                 height = 600)
     write_html(fig, "templates/marriage.html")
 
-def make_household_table(): 
+def make_household_table(state): 
     household_type = get_tables(['B11001_001E', 'B11001_002E','B11001_003E','B11001_004E','B11001_007E','B11001_008E','B11001_009E'], 
                                 ['Total', 'Total Family','Married-couple Family', 'Single Householder, no spouse','Total Nonfamily','Nonfamily Living Alone','Nonfamily Not Alone'], 
                                 [2009, 2014, 2019])
-    household_cal = household_type[household_type['State']=="California"]
-    mc_percentage = household_cal['Married-couple Family'] / household_cal['Total']
-    sh_percentage = household_cal['Single Householder, no spouse'] / household_cal['Total']
-    nla_percentage = household_cal['Nonfamily Living Alone'] / household_cal['Total']
-    nna_percentage = household_cal['Nonfamily Not Alone'] / household_cal['Total']
-    year = household_cal['Year']
+    household = household_type[household_type['State']==state]
+    mc_percentage = household['Married-couple Family'] / household['Total']
+    sh_percentage = household['Single Householder, no spouse'] / household['Total']
+    nla_percentage = household['Nonfamily Living Alone'] / household['Total']
+    nna_percentage = household['Nonfamily Not Alone'] / household['Total']
+    year = household['Year']
 
     household_percentage = pd.DataFrame({
         'Married-couple Family': mc_percentage,
@@ -105,25 +110,25 @@ def make_household_table():
     fig = px.bar(household_percentage,
              x="Year", 
              y=["Married-couple Family","Single Householder", "Nonfamily Living Alone","Nonfamily Not Alone"],  
-             title="Household Types (CA)", 
+             title="Household Types (" + state + ")", 
              width = 800, 
              height = 600)
 
     write_html(fig, "templates/household.html")
 
-def make_divorce_table(): 
+def make_divorce_table(state): 
     divorces = get_tables(['B12503_001E','B12503_003E','B12503_005E','B12503_006E','B12503_008E','B12503_010E','B12503_011E'],
                           ['Total','Male Never Married', 'Male Married; Divorced Last Year', 'Male Married; Not Divorced Last Year','Female Never Married','Female Married; Divorced Last Year','Female Married; Not Divorced Last Year'],
                           [2012, 2019])
 
-    divorces_cal = divorces[divorces['State']=='California']
-    never_married_total = divorces_cal["Male Never Married"] + divorces_cal["Female Never Married"]
-    div_last_year = divorces_cal["Male Married; Divorced Last Year"] + divorces_cal["Female Married; Divorced Last Year"]
-    married_not_div = divorces_cal["Male Married; Not Divorced Last Year"] + divorces_cal["Female Married; Not Divorced Last Year"]
-    total = divorces_cal["Total"]
-    year = divorces_cal["Year"]
+    divorces = divorces[divorces['State']==state]
+    never_married_total = divorces["Male Never Married"] + divorces["Female Never Married"]
+    div_last_year = divorces["Male Married; Divorced Last Year"] + divorces["Female Married; Divorced Last Year"]
+    married_not_div = divorces["Male Married; Not Divorced Last Year"] + divorces["Female Married; Not Divorced Last Year"]
+    total = divorces["Total"]
+    year = divorces["Year"]
 
-    divorces_cal_totals = pd.DataFrame({
+    divorces_totals = pd.DataFrame({
         'Total': total,
         'Never Married Total': never_married_total,
         'Ever Married; Divorced Last Year': div_last_year, 
@@ -131,22 +136,22 @@ def make_divorce_table():
         'Year': year
     })
 
-    never_married_per = divorces_cal_totals["Never Married Total"] / divorces_cal_totals["Total"]
-    married_div_per = divorces_cal_totals["Ever Married; Divorced Last Year"] / divorces_cal_totals["Total"]
-    married_not_div_per = divorces_cal_totals["Ever Married; Did Not Divorce Last Year"] / divorces_cal_totals["Total"]
+    never_married_per = divorces_totals["Never Married Total"] / divorces_totals["Total"]
+    married_div_per = divorces_totals["Ever Married; Divorced Last Year"] / divorces_totals["Total"]
+    married_not_div_per = divorces_totals["Ever Married; Did Not Divorce Last Year"] / divorces_totals["Total"]
 
-    divorces_cal_percentages = pd.DataFrame({
+    divorces_percentages = pd.DataFrame({
         'Never Married': never_married_per,
         'Ever Married; Divorced Last Year': married_div_per, 
         'Ever Married; Did Not Divorce Last Year': married_not_div_per, 
         'Year': year
     })
 
-    divorces_cal_percentages = divorces_cal_percentages.round(decimals = 4)
-    fig = px.scatter(divorces_cal_percentages, 
+    divorces_percentages = divorces_percentages.round(decimals = 4)
+    fig = px.scatter(divorces_percentages, 
                 x = "Year", 
                 y = ["Never Married", "Ever Married; Divorced Last Year", "Ever Married; Did Not Divorce Last Year"],
-                title = "Divorces in the Past Year in CA",
+                title = "Divorces in the Past Year in " + state,
                 trendline = "ols", # ordinary least squares regression trendline
                 width = 800,
                 height = 600)
@@ -164,5 +169,5 @@ def make_divorce_table():
 #Establish view page
 @app.route("/view")
 def view():
-    make_marriage_table()
+    make_marriage_table("California")
     return render_template('marriage.html')
